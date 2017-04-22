f017a6dd5b149f1a780f7d77aeb42ea9f701b69a
ShiannEscamillaP <shiann8815@live.com.mx>
2017-04-22 11:32:06 -0500

    gastos

--- ventanaGasto.java
+++ ventanaGasto.java
@@ -1,5 +1,18 @@
 package PFinal;
 
-public class ventanaGasto {
+import javax.swing.JFrame;
 
+public class ventanaGasto extends JFrame{
+
+	public ventanaGasto(){
+		super("Gastos");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		this.setResizable(false);
+		this.setLocation(800, 300);
+		
+		
+		this.pack();
+		this.setVisible(true);
+	}
 }
